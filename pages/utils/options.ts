457c73faa81be711de48f3faa3bef8883b0d4653
Luan <j0hn04v@gmail.com>
2023-01-16 19:44:22 -0300

    Initial commit

---
+++ pages/utils/options.ts
@@ -0,0 +1,18 @@
+export const options = [
+    {
+        name: "UpperCase",
+        value: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    },
+    {
+        name: "LowerCase",
+        value: "abcdefghijklmnopqrstuvwxyz",
+    },
+    {
+        name: "Digits",
+        value: "0123456789",
+    },
+    {
+        name: "Special",
+        value: "!\"#$%&'*+-_,./:;=?@\\^`|~[]{}()<>",
+    },
+];
